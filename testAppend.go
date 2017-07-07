5c012e4516b0537659e05ec1bfadf46d92da4752
xie <xie@xiedeair.lan>
2017-07-07 14:09:37 +0800

    测试了切片即使在容量内，也不能直接用越界的索引赋值

--- testAppend.go
+++ testAppend.go
@@ -13,10 +13,13 @@ func main(){
 		t := fmt.Sprintf("ACT-%s", v)
 		b = append(b, t)
 	}
-	
+	fmt.Println(len(b))
+    // b[3] = "hello"  //即使声明了容量，也不能直接通过索引来赋值
 	for _, v :=range(b){
 		fmt.Println(v)
 		fmt.Println(strings.Split(v, "-")[1])
 	}
 	fmt.Println(len(b))
+   
+    
 }
